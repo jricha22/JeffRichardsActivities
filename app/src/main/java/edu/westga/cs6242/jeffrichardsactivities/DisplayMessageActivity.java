077d8438032580f3ef6b6e07a0407122ad1c6d4f
Jeff Richards <jeff.richards@earthlink.net>
2016-01-17 12:48:28 -0500

    Fix issue with adding message to layout

--- app/src/main/java/edu/westga/cs6242/jeffrichardsactivities/DisplayMessageActivity.java
+++ app/src/main/java/edu/westga/cs6242/jeffrichardsactivities/DisplayMessageActivity.java
@@ -1,11 +1,14 @@
 package edu.westga.cs6242.jeffrichardsactivities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -28,5 +31,13 @@ public class DisplayMessageActivity extends AppCompatActivity {
         {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.display_message_layout);
+        layout.addView(textView);
     }
 }
